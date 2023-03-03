81c7ebac4e1a4561d86d9aa0e160a82615be1622
3dharmadev <69399810+3dharmadev@users.noreply.github.com>
2023-03-04 00:06:56 +0530

    1st commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          // ignore: avoid_unnecessary_containers
+          child: Container(
+            child: const Text("Welcome to flutter"),
+          ),
+        ),
+      ),
+    );
+  }
+}
